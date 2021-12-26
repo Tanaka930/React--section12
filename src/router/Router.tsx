8a59349628c37d3f1154c77483eaa1560ad01d4a
Tanaka930 <55372083+Tanaka930@users.noreply.github.com>
2021-12-26 19:45:48 +0900

    68

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,12 +4,21 @@ import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { HomeRoutes } from "./HomeRoutes";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templetes/HeaderLayout";
 
 export const Router: VFC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home/*" element={<HomeRoutes />} />
+
+      <Route
+        path="/home/*"
+        element={
+          <HeaderLayout>
+            <HomeRoutes />
+          </HeaderLayout>
+        }
+      />
       <Route path="*" element={<Page404 />} />
     </Routes>
   );
