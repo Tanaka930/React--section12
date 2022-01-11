a9098c9e78160dd2d280974e454452543f0fd9d3
Tanaka930 <55372083+Tanaka930@users.noreply.github.com>
2022-01-11 20:22:51 +0900

    81 form, 権限判定

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -19,7 +19,7 @@ export const UserManagement: VFC = memo(() => {
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
+  // console.log(loginUser);
 
   useEffect(() => getUsers(), []);
 
@@ -51,7 +51,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
